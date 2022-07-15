2d14bd539bf54dba7045f22088b2744432e5e1e8
Kyle Chen <Kyle.tk.Chen@viewsonic.com>
2022-07-15 16:22:39 +0800

    Initial Commit

---
+++ FactoryDesignPatternDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FactoryDesignPatternDemo
+//
+//  Created by Kyle Chen on 2022/7/15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
